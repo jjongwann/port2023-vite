6ec52c0d7dab292769b9d87841a8d0e1537764cc
Jongs <143797909+jjongwann@users.noreply.github.com>
2023-12-12 19:57:43 +0900

    헤더 영역 완료

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,7 +3,7 @@ import { port } from "./port.js";
 import { link } from "./link.js";
 import { smooth } from "./smooth.js";
 
-window.addEventListener("load", function () {
+window.addEventListener("load", function () { //window가 load되면 함수 실행!
     smooth();
     link();
     menu();
